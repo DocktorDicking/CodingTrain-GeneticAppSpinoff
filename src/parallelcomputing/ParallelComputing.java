53a08e958f42bd1c7111c25eb08df04738cf2768
Tom Scholten <Tom.Scholten2@hva.nl>
2018-05-17 15:03:10 +0200

    Initial commit

---
+++ src/parallelcomputing/ParallelComputing.java
@@ -0,0 +1,14 @@
+package parallelcomputing;
+
+/**
+ * @Author: Tom Scholten
+ * @Project Name: ParallelComputing
+ * @Function: ParallelComputing
+ * @Date: May 17, 2018 2:55:39 PM
+ * @version: 1.0
+ */
+public class ParallelComputing {
+    public static void main(String[] args) {
+        
+    }
+}
