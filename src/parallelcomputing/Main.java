fd6b5eebef511fd40fb9dbb1346f3a165458e2a2
Jim van Wieringen <jim.van.wieringen@hva.nl>
2018-05-21 22:19:20 +0200

    Done for today

--- src/parallelcomputing/Main.java
+++ src/parallelcomputing/Main.java
@@ -29,19 +29,7 @@ public class Main {
         /**
          * To Do List:
          *
-         * DONE: setUp()
-         * - Initialize: Create a population of N elements, each with randomly generated DNA
-         *
-         * DONE: Draw()
-         * - Selection: Evaluate the fitness of each element of the population and build a mating pool.
-         * - Reproduction: Repeat N times:
-         *
-         * a) Pick two parents with probability according to relative fitness.
-         * b) Crossover - create a "child" by combining the DNA of these two parents.
-         * c) Mutation - mutate the child's DNA based on a given probability.
-         * d) Add the new child to a new population.
-         *
-         * Finally replace old population with the new population and return to step 2.
+         * TODO Run application...
          *
          */
 
